9513c224f6082b29c1d936f8a88d1a081e5f76ef
Dmytro Novikov <dimmu.neivan@gmail.com>
2025-11-30 20:40:57 +0200

    Fix info window direction symbols.

--- src/info.cpp
+++ src/info.cpp
@@ -3,25 +3,25 @@
 Info::Info() {}
 
 void Info::show_info(WINDOW *win, unsigned int speed, unsigned int distance, int direction) {
-    char direction_symbol;
+    wchar_t direction_symbol;
 
     switch (direction) {
         case -1:
-            direction_symbol = '<';
+            direction_symbol = WCHAR_ARROW_LEFT;
             break;
 
         case 0:
         default:
-            direction_symbol = '^';
+            direction_symbol = WCHAR_ARROW_UP;
             break;
 
         case 1:
-            direction_symbol = '>';
+            direction_symbol = WCHAR_ARROW_RIGHT;
             break;
     }
 
     mvwprintw(win, 1, 1, "Speed: %d0 km/h", speed);
-    mvwprintw(win, 2, 1, "Direction: %c", direction_symbol);
+    mvwprintw(win, 2, 1, "Direction: %lc", direction_symbol);
     mvwprintw(win, 3, 1, "Distance: %d", distance);
 
     mvwprintw(win, 5, 1, "Use %lc, %lc, %lc", WCHAR_ARROW_LEFT, WCHAR_ARROW_UP, WCHAR_ARROW_RIGHT);
